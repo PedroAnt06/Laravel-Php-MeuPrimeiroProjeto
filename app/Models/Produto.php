6f32ca8d0a31ac89cb7b6c26e71d29cbbfa9078e
Pedro Antonio <pedroantoniolopesdearruda6@gmail.com>
2026-06-11 22:51:54 -0300

    docs: add coments (model: produto)

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Produto extends Model
+class Produto extends Model //propriamente para gerenciar os dados do banco de dados, pelo Eloquent ORM do Laravel
 {
-    protected $table = "produtos";
+    protected $table = "produtos"; // Especifica o nome da tabela no banco de dados
 
-    protected $fillable = [
+    protected $fillable = [ //regra de seguranca para evitar mass assignment - que o cliente nao envie campos que n devem ser preenchidos
         'nome',
         'preco',
         'quantidade'
